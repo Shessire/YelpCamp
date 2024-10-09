d77069fd4c23a2d9430e8a8dd6c30045846b0d09
wisuwat <wisuwatpond01@gmail.com>
2024-10-09 16:22:05 +0700

    Create Model

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path')
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+  console.log("CONNECTED TO MONGODB")  
+}
+
+const app = express();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
