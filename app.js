f9759341be44163971a0216d91359f3f6472b791
wisuwat <wisuwatpond01@gmail.com>
2024-10-12 20:45:25 +0700

    Edit & Update Campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 main().catch(err => console.log(err));
@@ -16,6 +17,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req,res) => {
     res.render('home')
@@ -43,6 +45,17 @@ app.get('/campgrounds/:id', async(req,res) => {
   res.render('campgrounds/show', { campgrounds })
 })
 
+app.get('/campgrounds/:id/edit', async(req,res) => {
+  const { id } = req.params;
+  const campgrounds = await Campground.findById(id);
+  res.render('campgrounds/edit', {campgrounds});
+})
+
+app.put('/campgrounds/:id', async(req,res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
     console.log("CONNECTED TO PORT 3000")
