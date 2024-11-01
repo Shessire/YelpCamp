572b4b623c69410b9b01eb11bfd233b7256557e2
wisuwat <wisuwatpond01@gmail.com>
2024-11-01 15:10:12 +0700

    Add Basic Error Handler

--- app.js
+++ app.js
@@ -34,11 +34,15 @@ app.get('/campgrounds/new',(req,res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async(req,res) => {
+app.post('/campgrounds', async(req,res,next) => {
+  try {
   const { campground } = req.body;
   const newCampground = new Campground(campground)
   await newCampground.save();
   res.redirect(`/campgrounds/${newCampground._id}`)
+  } catch(e){
+    next(e);
+  }
 })
 
 app.get('/campgrounds/:id', async(req,res) => {
@@ -63,6 +67,10 @@ app.delete('/campgrounds/:id', async(req,res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+})
+
+app.use((err,req,res,next) => {
+  res.send("Something went wrong")
 })
 
 app.listen(3000, () => {
