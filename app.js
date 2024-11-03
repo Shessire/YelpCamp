4a92ce8e8901c55484a5166d871c2c4259f35492
wisuwat <wisuwatpond01@gmail.com>
2024-11-03 17:35:26 +0700

    More Errors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -37,6 +38,7 @@ app.get('/campgrounds/new',(req,res) => {
 
 app.post('/campgrounds', catchAsync (async(req,res,next) => {
   const { campground } = req.body;
+  if(!campground) throw new ExpressError('Invalid Campground Data', 400)
   const newCampground = new Campground(campground)
   await newCampground.save();
   res.redirect(`/campgrounds/${newCampground._id}`)
@@ -66,8 +68,13 @@ app.delete('/campgrounds/:id', catchAsync (async(req,res) => {
   res.redirect('/campgrounds');
 }))
 
+app.all('*', (req,res,next) => {
+  next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err,req,res,next) => {
-  res.send("Something went wrong")
+  const { statusCode = 500, message = 'Something went wrong'} = err;
+  res.status(statusCode).send(message)
 })
 
 app.listen(3000, () => {
